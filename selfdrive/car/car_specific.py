ca1e114020c46c98e4dbe9736f6db7ff94d39a3f
AbhishekMitra-AIT <abhishekmitra91@gmail.com>
2025-02-10 15:12:40 +0530

    resetting selfdrive

--- selfdrive/car/car_specific.py
+++ selfdrive/car/car_specific.py
@@ -45,9 +45,6 @@ class CarSpecificEvents:
     if self.CP.brand in ('body', 'mock'):
       events = Events()
 
-    elif self.CP.brand in ('byd', 'subaru', 'mazda'):
-      events = self.create_common_events(CS, CS_prev)
-
     elif self.CP.brand == 'ford':
       events = self.create_common_events(CS, CS_prev, extra_gears=[GearShifter.manumatic])
 
